7cce091a551be5d71e12a7d938cbcdbd9664fa8d
Coker <gbolahancoker99@gmail.com>
2023-12-13 14:41:20 +0100

    edited navigation

--- src/component/MovieDisplay/MovieDisplay.jsx
+++ src/component/MovieDisplay/MovieDisplay.jsx
@@ -1,48 +1,85 @@
-import React from 'react'
-import { PiTelevision  } from 'react-icons/pi'
+import React, { useState } from "react";
+import { PiTelevision } from "react-icons/pi";
 import { IoMdHome } from "react-icons/io";
 import { RiMovieFill } from "react-icons/ri";
 import { PiTelevisionSimpleLight } from "react-icons/pi";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { CiLogin } from "react-icons/ci";
-import { Link } from 'react-router-dom'
-import MovieAbout from './MovieAbout';
+import { Link } from "react-router-dom";
+import MovieAbout from "./MovieAbout";
+import { MdOutlineMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+
 const MovieDisplay = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle((prevClick) => !prevClick);
+  };
+
   return (
     <React.Fragment>
-      <header id='logos'>
-        <div id="logo">
+      <header className="flex  lg:flex-col lg:gap-10 items-center justify-between  lg:items-start px-5">
+        <div id="logo" className="lg:fixed z-10">
           <PiTelevision />
-          <p >MovieBox</p>
+          <p>MovieBox</p>
         </div>
-        <nav>
-          <ul>
-            <li>
-            <Link to="/"> < IoMdHome className='md:text-2xl sm:text-xl'/> Home</Link>
-            </li>
-            <li>
-              <Link to="#">< RiMovieFill  className=' md:text-2xl sm:text-xl'/> Movies</Link>
-            </li>
-            <li>
-              <Link to="#"><PiTelevisionSimpleLight  className='md:text-2xl sm:text-xl'/> Tv Series</Link>
-            </li>
-            <li>
-              <Link to="#"> < FaRegCalendarAlt  className='md:text-2xl sm:text-xl'/> Upcoming</Link>
-            </li>
-            <div id="movie_play">
-              <p>Play Mmovie quizes and earn free tickets</p>
-              <p>50k people are playing now</p>
-              <p className='pin'>Start Playing</p>
+
+        <div  className="lg:flex lg:justify-start flex-col pt-32 gap-10 pl-6 text-2xl hidden border border-black rounded-r-lg h-screen fixed left-0 w-[280px]"  id="log">
+         <Link to={'/'}>
+         <p className="flex items-center gap-2">
+            <IoMdHome /> Home
+          </p>
+         </Link>
+          <p className="flex items-center gap-2">
+            <IoMdHome /> Home
+          </p>
+          <p className="flex items-center gap-2">
+            <IoMdHome /> Home
+          </p>
+          <p className="flex items-center gap-2">
+            <IoMdHome /> Home
+          </p>
+          <div className="bg-red-600 w-fit h-40">
+            fghjkldkfjh
+          </div>
+          <p className="flex items-center gap-2">
+            <IoMdHome /> Home
+          </p>
+        </div>
+
+        <div className="relative lg:hidden  ">
+          <div className=" text-2xl md:text-4xl cursor-pointer">
+            {!toggle ? (
+              <MdOutlineMenu onClick={handleClick} />
+            ) : (
+              <IoMdClose onClick={handleClick} />
+            )}
+          </div>
+          {toggle && (
+            <div className="absolute h-[50vh] justify-evenly flex flex-col gap-5 text-lg md:text-2xl  w-[100vw] py-4 px-10 right-0 m-0 bg-white">
+              <p className="flex items-center gap-2">
+                <IoMdHome /> Home
+              </p>
+              <p className="flex items-center gap-2">
+                <IoMdHome /> Home
+              </p>
+              <p className="flex items-center gap-2">
+                <IoMdHome /> Home
+              </p>
+              <p className="flex items-center gap-2">
+                <IoMdHome /> Home
+              </p>
+              <p className="flex items-center gap-2">
+                <IoMdHome /> Home
+              </p>
             </div>
-            <li>
-              <Link to="#"> <CiLogin  className='md:text-2xl sm:text-xl'/>Log Out</Link>
-            </li>
-          </ul>
-        </nav>
+          )}
+        </div>
       </header>
       <MovieAbout />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default MovieDisplay
+export default MovieDisplay;
